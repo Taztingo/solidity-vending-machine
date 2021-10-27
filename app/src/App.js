4a6d1ab21c3296609e773c63869d8214096515d9
Matthew Witkowski <taztingo503@gmail.com>
2021-10-26 23:33:29 -0400

    Added logic for VendingMachine to display items dynamically.

--- app/src/App.js
+++ app/src/App.js
@@ -5,13 +5,29 @@ import {useState} from 'react';
 
 function App() {
   const [isOwner, setOwner] = useState(false);
+  const [items, setItems] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13
+  ]);
+  const MAX_COLUMNS = 4;
 
   return (
     <div className="App">
         <Header isOwner={isOwner}/>
         <Container>
           <Row>
-            <Col><VendingMachine/></Col>
+            <Col><VendingMachine items={items} columns={MAX_COLUMNS}/></Col>
           </Row>
         </Container>
     </div>
--- app/src/components/VendingMachine.js
+++ app/src/components/VendingMachine.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
+import {Row, Col, Card} from 'react-bootstrap';
 
-const VendingMachine = () => {
+const VendingMachine = ({items, columns}) => {
     return (
         <div className="VendingMachine">
-            
+            {[...Array(Math.ceil(items.length / columns)).keys()]
+                .map(i => items.slice(i * columns, (i+1) * columns))
+                .map(row => {
+                    return (<Row>
+                        {row.map(col => {
+                            return (
+                            <Col md={Math.ceil(12/columns)}>
+                                <Card>
+                                    <Card.Body>
+                                    <Card.Title>Card title</Card.Title>
+                                    <Card.Text>
+                                        This is a longer card with supporting text below as a natural
+                                        lead-in to additional content. This content is a little bit longer.
+                                    </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>);
+                        })}
+                    </Row>)
+                }
+            )}
         </div>
     )
 }
